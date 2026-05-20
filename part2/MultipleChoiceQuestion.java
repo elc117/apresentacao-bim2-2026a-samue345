f64e8fa0740579a5857d16f4a6a69ee35dc20cbf
samue345 <samuef339@gmail.com>
2026-05-19 22:53:10 -0300

    feature: criando classe MultipleChoiceQuestion

---
+++ part2/MultipleChoiceQuestion.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class MultipleChoiceQuestion extends Question {
+    private List<String> options;
+
+    public MultipleChoiceQuestion(String statement, List<String> options, String correctAnswer) {
+        super(statement, correctAnswer);
+        this.options = options;
+    }
+
+    @Override
+    public void showQuestion() {
+        System.out.println("Múltipla escolha:");
+        System.out.println(statement);
+
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println((i + 1) + ") " + options.get(i));
+        }
+    }
+}
